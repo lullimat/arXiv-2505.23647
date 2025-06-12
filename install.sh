bc4ffe2344b9d20a07c83862f11a83506a802656
lullimat <matteo.lulli@gmail.com>
2025-06-12 10:43:25 +0800

    successful sourcing aliases from .bashrc

--- install.sh
+++ install.sh
@@ -6,12 +6,22 @@
 echo "Installing the idea.deploy environment for the repository of the paper:"
 echo "'Higher-order Tuning of Interface Physics in Multiphase Lattice Boltzmann'"
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/lullimat/idea.deploy/refs/heads/master/idpy-bootstrap.sh)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/lullimat/idea.deploy/refs/heads/master/idpy-bootstrap.sh)"
 
+# Sourcing the .bashrc
+shopt -s expand_aliases
 source ${HOME}/.bashrc
+IDPY_GO="$(type -a idpy-go | cut -d '`' -f 2 | cut -d \' -f 1)"
+IDPY_LOAD="$(type -a idpy-load | cut -d '`' -f 2 | cut -d \' -f 1)"
 idpy-go
 idpy-load
+cd ./papers/
+echo ${PWD}
 
-# cd ./papers/
-# python idpy-papers.py arXiv-2505.23647
-# cd ./arXiv-2505.23647/
+python idpy-papers.py --repo arXiv-2505.23647
+cd ./arXiv-2505.23647/
+echo
+echo "The environemnt idea.deploy is ready and the paper repository has been succefully cloned!"
+echo
+echo "Please run 'cd ${PWD}' to go to the paper repository"
+echo
